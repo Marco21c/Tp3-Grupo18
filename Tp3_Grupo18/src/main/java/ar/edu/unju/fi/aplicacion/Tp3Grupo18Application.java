4844a67a42a924d547ee6035fcf489278b4b66c8
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-04-21 00:07:18 -0300

    Comienzo de tp3

---
+++ Tp3_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Tp3Grupo18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Grupo18Application.class, args);
+	}
+
+}
